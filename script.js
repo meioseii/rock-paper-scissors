6d9ce8eb479ec2a057cc172b3296b5ae246c5260
meioseii <moreno.austin12@gmail.com>
2022-06-15 09:39:07 +0800

    function playerPlay added, gets the choice of the player rpp.

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
+const playerSelection = playerPlay()
+const computerSelection = computerPlay()
 
 function computerPlay() {
-    let computerChoice = ["Rock", "Paper", "Scissors"];
-    return computerChoice[Math.floor(Math.random(computerChoice.length - 1)*3)]
+    const computerChoice = ["Rock", "Paper", "Scissors"];
+    return computerChoice[Math.floor(Math.random()*computerChoice.length)]
+}
+
+function playerPlay() {
+    let playerSelect = prompt("Rock, paper, or scissors?")
+    playerSelect = playerSelect.charAt(0).toUpperCase() + playerSelect.slice(1).toLowerCase();
 }
 
 function startRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissors")
-        return "You win! Rock beats scissors!";
+        return "You win! Rock beats Scissors!";
     else if (playerSelection === "Rock" && computerSelection === "Rock")
-        return "Draw! You both choose rock!";
+        return "Draw! You both choose Rock!";
     else if (playerSelection === "Rock" && computerSelection === "Paper")
-        return "You lose! Paper beats rock!";
+        return "You lose! Paper beats Rock!";
 
     else if (playerSelection === "Paper" && computerSelection === "Rock")
-        return "You win! Paper beats rock!";
+        return "You win! Paper beats Rock!";
     else if (playerSelection === "Paper" && computerSelection === "Paper")
-        return "Draw! You both choose paper!";
+        return "Draw! You both choose Paper!";
     else if (playerSelection === "Paper" && computerSelection === "Scissors")
-        return "You lose! Scissors beats paper!"
+        return "You lose! Scissors beats Paper!"
         
     else if (playerSelection === "Scissors" && computerSelection === "Paper")
-        return "You win! Scissors beats paper!";
+        return "You win! Scissors beats Paper!";
     else if (playerSelection === "Scissors" && computerSelection === "Scissors")
-        return "Draw! You both choose scissors!";
+        return "Draw! You both choose Scissors!";
     else if (playerSelection === "Scissors" && computerSelection === "Rock")
         return "You lose! Rock beats Scissors!"
 }
 
-const computerSelection = computerPlay()
+function game() {
+    startRound();
+}
 
+console.log(startRound(playerSelection, computerSelection));
 
-console.log(startRound("Rock",computerSelection));
