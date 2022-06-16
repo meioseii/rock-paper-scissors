89c86fc7e5a3cfe7c38b85dca40e32422a28731f
meioseii <moreno.austin12@gmail.com>
2022-06-16 08:01:17 +0800

    playRound function added.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const computerSelection = computerPlay();
 const playerSelection = playerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let computerChoices = ["Rock", "Paper", "Scissors"]
@@ -12,6 +14,29 @@ function playerPlay() {
             return playerChoices;
 }
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection == computerSelection) {
+        return "Draw!"
+    }   else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        playerScore++
+            return "You win! Rock beats Scissors!"
+    }   else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        playerScore++
+            return "You win! Scissors beats Paper!"
+    }   else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        playerScore++
+            return "You win! Paper beats Rock!"
+    }   else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        computerScore++
+            return "You lose! Paper beats Rock!"
+    }   else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        computerScore++
+            return "You lose! Rock beats Scissors!"
+    }   else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        computerScore++
+            return "You lose! Scissors beats Paper!"
+    }
 }
 
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerScore);
+console.log(computerScore);
